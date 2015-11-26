e1f59de96051fdb1f2f4ffff67d9d9a83c939657
AndreaGhizzoni <andrea.ghz@gmail.com>
2015-11-26 11:24:47 +0100

    update 4 with multimple way to instance a struct

--- 4-Struct/struct.go
+++ 4-Struct/struct.go
@@ -1,4 +1,4 @@
-// Simple struct with method
+// Simple structure with method
 //
 //author: Andrea Ghizzoni
 //info:   andrea.ghz@gmail.com
@@ -10,12 +10,20 @@ type Point struct{
     x, y float64
 }
 
-func (p *Point) Abs() float64 {
-    return 1
+func (p *Point) Sum() float64 {
+    return p.x + p.y
 }
 
 func main(){
     p := &Point{ 1, 2 }
-    fmt.Printf("p.Abs() = %f\n", p.Abs())
+    fmt.Printf("p.Sum() = %f\n", p.Sum())
+
+    p1 := &Point{ x:1, y:2 }
+    fmt.Printf("p1.Sum() = %f\n", p1.Sum())
+
+    p2 := new(Point)
+    p2.x = 1
+    p2.y = 1
+    fmt.Printf("p2.Sum() = %f\n", p2.Sum())
 }
 
