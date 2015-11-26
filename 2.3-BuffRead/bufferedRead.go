1286ade2a374d04ed6fad8673d9d1a5ed4b676f1
AndreaGhizzoni <andrea.ghz@gmail.com>
2015-11-26 11:56:13 +0100

    adde 2.3

---
+++ 2.3-BuffRead/bufferedRead.go
@@ -0,0 +1,36 @@
+//
+//
+//author: Andrea Ghizzoni
+//info:   andrea.ghz@gmail.com
+package main
+
+import(
+    "fmt"
+    "flag"
+    "bufio"
+    "strings"
+    "os"
+)
+
+func main(){
+    path := flag.String("path", "", "absolute path of file to read")
+    flag.Parse()
+
+    if strings.EqualFold( *path, "" ) || len(os.Args) == 1 {
+        fmt.Printf("-path=/path/to/file missing\n")
+        return
+    }
+
+    f, err := os.Open(*path)
+    if err != nil{
+        panic(err)
+    }
+    reader := bufio.NewReader(f)
+    for {
+        line, err := reader.ReadString('\n')
+        fmt.Print(line)
+
+        if err != nil{ break }
+    }
+}
+
