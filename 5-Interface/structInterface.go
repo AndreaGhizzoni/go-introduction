67f2cfda444b5b8613f19f40bf5e06203a3d9181
AndreaGhizzoni <andrea.ghz@gmail.com>
2015-11-26 11:40:51 +0100

    added 5

---
+++ 5-Interface/structInterface.go
@@ -0,0 +1,28 @@
+// Simple use of Interface
+//
+//author: Andrea Ghizzoni
+//info:   andrea.ghz@gmail.com
+package main
+
+import "fmt"
+
+// interface declaration
+type SumInterface interface{
+    Sum() int
+}
+
+// structure declaration that implements SumInterface
+type Point struct {
+    x, y int
+}
+
+func (p *Point) Sum() int {
+    return p.x + p.y
+}
+
+func main(){
+    var si SumInterface
+    si = &Point{1,2}
+    fmt.Printf("SumInterface.Sum = %d\n", si.Sum())
+}
+
