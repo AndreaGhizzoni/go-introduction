1d678454ac91a308cac3a88488c65322f47033ca
AndreaGhizzoni <andrea.ghz@gmail.com>
2015-12-24 11:50:53 +0100

    added commenets on 0

--- 0-HelloWorld/hello.go
+++ 0-HelloWorld/hello.go
@@ -1,17 +1,24 @@
 //Basic hello world example.
 package main
 
+// import packages
 import(
     "fmt"
     "os"
     "strings"
 )
 
+// main function. NB: no return type and no passing arguments
 func main(){
+    // short variable declaration
     who := "World!"
+
+    // os.Args have access to arguments passing to main
     if len(os.Args) > 1 {
         who = strings.Join( os.Args[1:], " " )
     }
+
+    // print on standard output
     fmt.Printf( "Hello %s\n", who );
 }
 
