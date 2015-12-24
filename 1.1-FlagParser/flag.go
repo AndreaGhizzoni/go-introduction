12a21afdac27f3e6a219095f91ad33e128501b85
AndreaGhizzoni <andrea.ghz@gmail.com>
2015-12-24 12:00:05 +0100

    added comments in example 1

--- 1.1-FlagParser/flag.go
+++ 1.1-FlagParser/flag.go
@@ -1,4 +1,5 @@
-//Introduction to flag module
+// Introduction to flag module.
+// see golang.org/pkg/flag
 package main
 
 import (
@@ -7,11 +8,15 @@ import (
 )
 
 func main() {
+    // create a flag used to pass arguments to main
 	stringFlag := flag.String("str", "noStr", "a string flag value")
 	boolFlag := flag.Bool("bool", false, "a boolean flag value")
 	intFlag := flag.Int("int", -1, "a int flag value")
+
+    // parse main arguments
 	flag.Parse()
 
+    // use the value contained into previous declared pointer
 	fmt.Printf("str: %s\n", *stringFlag)
 	fmt.Printf("bool: %t\n", *boolFlag)
 	fmt.Printf("int: %d\n", *intFlag)
--- 1.2-Fibonacci/fib.go
+++ 1.2-Fibonacci/fib.go
@@ -1,15 +1,17 @@
-//Example of Go lang program to calculate n-Fibonacci number.
+// Example of Go lang program to calculate n-Fibonacci number.
 package main
 
 import (
     "flag"
     "fmt"
+    "os"
 )
 
-func Fibonacci( n int ) (int64) {
-    var f, s, t int64
+// This function calculate the n-Fibonacci number.
+func Fibonacci( n int64 ) (int64) {
+    var i, f, s, t int64
     f, s, t = 1, 1, 1
-    for i:=2; i<n; i++ {
+    for i=2; i<n; i++ {
         f = s
         s = t
         t = f+s
@@ -18,13 +20,15 @@ func Fibonacci( n int ) (int64) {
 }
 
 func main(){
-    p := flag.Int("of", -1, "the n-Fibonacci number to calculate")
+    // take the N as main argument 
+    p := flag.Int64("of", -1, "the n-Fibonacci number to calculate")
     flag.Parse()
 
+    // dereferencing pointer
     n := (*p)
     if n == -1 {
         fmt.Println("-of=<n> not found")
-        return
+        os.Exit(1);
     }
 
     fmt.Printf("Fibonacci(%v) = %v\n", n, Fibonacci(n) )
