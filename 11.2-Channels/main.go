7a767e52923e456fe156264d6d08a0db5af95f0c
AndreaGhizzoni <andrea.ghz@gmail.com>
2016-02-03 14:40:29 +0100

    added basic example of channels using

---
+++ 11.2-Channels/main.go
@@ -0,0 +1,21 @@
+// example of go channels
+package main
+
+import "fmt"
+
+// simple function that takes a channel of string as argument, then write a
+// string in it.
+func writeOnChannel(ch chan string) {
+	ch <- "hello World!"
+}
+
+func main() {
+	// create a channel of strings
+	messages := make(chan string)
+	// start a new go routine
+	go writeOnChannel(messages)
+
+	fmt.Print("Messages from go routine: ")
+	// retrieve the message from go routine
+	fmt.Println(<-messages)
+}
