016259ce3a23134bf48b98b28d951a302a13cec6
AndreaGhizzoni <andrea.ghz@gmail.com>
2016-02-15 13:52:25 +0100

    modilfy chapter 6

--- 6.1-Struct/struct.go
+++ 6.1-Struct/struct.go
@@ -1,31 +1,57 @@
-// Simple structure with method
+// Structure with methods
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-// structure declaration
-type Point struct {
+// structure declaration with two unexported field
+type positivePoint struct {
 	x, y float64
 }
 
-// Method associated with previous structure with (p *Point) declaration.
+// method used as constructor of positivePoint type.
+// No (p *positivePoint) delcaration needed because when this module is imported
+// the syntax to create an instance of it is
+// <module_name>.NewPositivePoint([...])
+func NewPositivePoint(x, y float64) (*positivePoint, error) {
+	if x < 0 || y < 0 {
+		return nil, errors.New("positivePoint accepts positive coordinates only.")
+	}
+	return &positivePoint{x, y}, nil
+}
+
+// Method associated with previous structure with (p *positivePoint) declaration.
 // p is know as "receiver" or implicit "this" parameter
-func (p *Point) Sum() float64 {
+func (p *positivePoint) Sum() float64 {
 	return p.x + p.y
 }
 
 func main() {
+	// note: the two ways below to construct a positivePoint are possible
+	// because positivePoint is declared in this package. If was declared
+	// elsewhere, these two ways can not be done because positivePoint.x and
+	// positivePoint.y are unexported.
+
 	// construct a pointer with unnamed filed
-	p := &Point{1, 2}
+	p := &positivePoint{1, 2}
 	fmt.Printf("p.Sum() = %f\n", p.Sum())
 
 	// construct a pointer with named filed
-	p1 := &Point{x: 1, y: 2}
+	p1 := &positivePoint{x: 1, y: 2}
 	fmt.Printf("p1.Sum() = %f\n", p1.Sum())
 
 	// construct a pointer with new() built-in function
-	p2 := new(Point)
-	p2.x = 1
+	p2 := new(positivePoint)
+	p2.x = 1 // assignment possible because see note above.
 	p2.y = 1
 	fmt.Printf("p2.Sum() = %f\n", p2.Sum())
+
+	// construct a positivePoint in the proper way
+	p3, err := NewPositivePoint(10.32, 23.42)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("p3.Sum() = %f\n", p3.Sum())
 }
--- 6.2-EmbeddingStruct/embedding.go
+++ 6.2-EmbeddingStruct/embedding.go
@@ -8,6 +8,11 @@ type NameObj struct {
 	Name string
 }
 
+// method of NameObj
+func (this *NameObj) foo() int {
+	return 42
+}
+
 // type Point
 type Point struct {
 	X, Y float64
@@ -22,6 +27,13 @@ type Square struct {
 	Name    string //-SHADOWED
 }
 
+// method of Square.
+// NOTE: without this method you can still write square.foo() but is called
+// square.NameObj.foo()
+func (this *Square) foo() int {
+	return 24
+}
+
 func main() {
 	// note that NameObj is directly initialized here, in real life is done by
 	// the constructor
@@ -33,11 +45,15 @@ func main() {
 	}
 
 	// square.Name is called shadowed because without it will point to
-	// square.NameObj.Name, the inherited structure
+	// square.NameObj.Name, the inherited structure.
+	// The same trick is applied with foo() method
 	fmt.Println("Accessing to shadowed field of inherited:", square.Name)
 	// but is still possible to access directly to the inherited field
 	fmt.Println("Accessing to inherited field:", square.NameObj.Name)
 	// access to other field
 	fmt.Printf("Accessing to square.Point field: %.3f %.3f\n", square.Center.X, square.Center.Y)
 	fmt.Printf("Accessing to square.Side field: %.3f\n", square.Side)
+
+	// calling foo() method
+	fmt.Printf("The answer is %d not %d\n", square.NameObj.foo(), square.foo())
 }
